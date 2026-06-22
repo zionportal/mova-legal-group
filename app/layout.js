49cf95ea9e7010233782e2b4df73d5655ef1314f
David Morgan <captainmorgan.3000@gmail.com>
2026-06-22 11:26:00 -0500

    Use three-column services grid

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import './cta.css';
+import './practice-grid.css';
 import { Cormorant_Garamond, Montserrat } from 'next/font/google';
 
 const display = Cormorant_Garamond({ subsets: ['latin'], variable: '--font-display', weight: ['500','600','700'] });
